ae160694386923c1dc535c99d66fdc8f0689d0fc
Patrick Carter <xCarter93@gmail.com>
2023-07-17 18:29:01 -0400

    Added routing to shop page as well as initial styling of page.  Added ProductContext which allows us to track products and use in different components.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
+import Shop from "./routes/shop/shop.component";
 
 const App = () => {
 	return (
@@ -10,6 +11,7 @@ const App = () => {
 				<Route path="/" element={<Navigation />}>
 					<Route index element={<Home />} />
 					<Route path="auth" element={<Authentication />} />
+					<Route path="shop" element={<Shop />} />
 				</Route>
 			</Routes>
 		</>
